56f3fa937370fb0033f7e7bb24d330de7b01867c
niloykar005 <niloykar005@gmail.com>
2026-04-05 21:00:54 +0100

    Added TreeNode implementation

--- Classes/DataStructures/TreeNode.cs
+++ Classes/DataStructures/TreeNode.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+using Takeaway_Restaurant_Management_System.Classes.Models;
+
+namespace Takeaway_Restaurant_Management_System.Classes.DataStructures
+{
+    public class TreeNode
+    {
+        public MenuItemModel Item { get; set; }
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+
+        public TreeNode(MenuItemModel item)
+        {
+            Item = item;
+            Left = null;
+            Right = null;
+        }
+    }
+}
