c5d9765d85bec72c3e2544df71a98721b2044edc
niloykar005 <niloykar005@gmail.com>
2026-04-06 03:19:04 +0100

    Program.cs added

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using System.Windows.Forms;
+using Takeaway_Restaurant_Management_System.Forms;
+
+namespace Takeaway_Restaurant_Management_System
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmLogin());
+        }
+    }
+}
